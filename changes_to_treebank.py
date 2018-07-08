36630d812a390d1a785a49f257f6d1b39eb3dce9
Shoval <shovatz@gmail.com>
2018-07-08 17:55:56 +0300

    last fixes of treebanks

--- changes_to_treebank.py
+++ changes_to_treebank.py
@@ -20,11 +20,16 @@ def suit_for_pandas(filepath):
         if 'sent_id' in line:
             sent_id = "".join([x for x in line if x.isnumeric()])
         if line[0] == '#':
-            output.write('%s\t\t\t\t\t\t\t\t\t\n' % line)
-        else:
+            output.write(line + "\t"*9 + "\n")
+        elif line.strip() != '':
+            # output.write(sent_id + ',' + line)
             output.write(line)
 
-
+def add_sentence(data):
+    data = data.assign(SENTENCE='_')
+    data['SENTENCE'] = data['INDEX'].str.split(',').str[0]
+    data['INDEX'] = data['INDEX'].str.split(',').str[1]
+    return data
 
 
 def get_elements(data, col_name, value):
@@ -95,23 +100,14 @@ def get_context(data, dependent):
     sentence = dependent
     while '#' not in str(sentence['INDEX']):
         sentence = data.iloc[int(sentence.name) - 1]
-    context = [dependent, get_head(data, dependent), sentence['INDEX']]
+    context = [dependent['FORM'], sentence['INDEX']]
     return context
 
-
-def change_dependent(data, extract_col, extract_value, old_dependent_rel, new_dependent_rel, head_rel):
-    name = 0
-    advs = get_elements(data, extract_col, extract_value)
-    for i, v in advs.iterrows():
-        if (get_head(data, v)['DEPREL'] == head_rel) and (v['DEPREL'] == old_dependent_rel):
-            data.at[v.name, 'DEPREL'] = new_dependent_rel
-    return data
 
 
 
 
 def flip_aux_xcomp_for_modals(data):
-
     deps = get_elements(data, 'DEPREL', 'xcomp')
     for i, v in deps.iterrows():
         head = get_head(data, v)
@@ -123,16 +119,66 @@ def flip_aux_xcomp_for_modals(data):
             deps.loc[i, 'HEAD'] = head['HEAD']
             data.loc[head.name, 'HEAD'] = v['INDEX']
             data.loc[i, 'HEAD'] = deps.loc[i, 'HEAD']
-
-            # v['HEAD'] = head['HEAD']
-            # head['HEAD'] = v['INDEX']
     return data
+
+def change_dependent(data, extract_col, extract_value, col, old_dependent_value, new_dependent_value, head_value):
+    name = 0
+    advs = get_elements(data, extract_col, extract_value)
+    for i, v in advs.iterrows():
+        if (get_head(data, v)[col] == head_value) and (v[col] == old_dependent_value):
+            data.at[v.name, col] = new_dependent_value
+    return data
+
+def predicative_complements_to_xcomp(data):
+    """
+    UD guidelines tag predicative complemets as xcomp.
+    examples:
+    you look great - 'look' is root and 'great' is xcomp
+    we expected them to change their minds - expected is root, them is obj (on expected) and change is xcomp (on expected)
+
+    update: changing the latter case is super specific and also more complicated (as the verb is the head of the head and
+    it's impossible to validate the intermediate noun), so for the moment it is aborted.
+    :param data:
+    :return:
+    """
+    adjs = get_elements(data, 'UPOSTAG', 'ADJ')
+    for index, v in adjs.iterrows():
+        if get_head(data, v)['UPOSTAG'] == 'VERB' and v['DEPREL'] in ['advcl', 'advmod']:
+            data.at[v.name, 'DEPREL'] = 'xcomp'
+    # verbs = get_elements(data, 'UPOSTAG', 'VERB')
+    # for index, v in verbs.iterrows():
+    #     if v['DEPREL'] == 'acl':
+    #         head = get_head(data, v)
+    #         head_of_head = get_head(data, head)
+    #         if head_of_head['UPOSTAG'] == 'VERB' and head['DEPREL'] == 'obj' and head['UPOSTAG'] == 'PRON':
+    #             print(get_context(data, v))
+    #             print(head_of_head['FORM'])
+    #             print(v['FORM'], v['DEPREL'])
+
+    return data
+
+def advmod_phrase_to_fixed(data):
+    """
+    In case of advmod:phrase, make its dependent's relation 'fixed' instead of advmod or dep
+    :param data:
+    :return:
+    """
+    advmods = get_elements(data, 'DEPREL', 'advmod')
+    deps = get_elements(data, 'DEPREL', 'dep')
+    for i, v in advmods.iterrows():
+        if get_head(data, v)['DEPREL'] == 'advmod:phrase':
+            data.at[v.name, 'DEPREL'] = 'fixed'
+    for i, v in deps.iterrows():
+        if get_head(data, v)['DEPREL'] == 'advmod:phrase':
+            data.at[v.name, 'DEPREL'] = 'fixed'
+    return data
+
 
 
 def make_changes(filepath):
     suit_for_pandas(filepath)
     data = pd.read_csv("modified_%s" % filepath, sep='\t', quoting=csv.QUOTE_NONE)
-    # data.columns = ["SENTENCE", "INDEX", "FORM", "LEMMA", "UPOSTAG", "XPOSTAG", "FEATS", "HEAD", "DEPREL", "DEPS", "MISC"]
+    # data = add_sentence(data)
     data = naive_change_value(data, 'DEPREL', 'iobj', 'obl')
     data = naive_change_value(data, 'DEPREL', 'acl:inf', 'acl')
     data = naive_change_value(data, 'DEPREL', 'det:quant', 'det')
@@ -149,33 +195,15 @@ def make_changes(filepath):
     data = naive_change_value(data, 'XPOSTAG', 'PART', 'ADP')
     data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'INTJ', 'advmod', new_col2_value='discourse')
     data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'INTJ', 'advmod', new_col2_value='discourse')
-    data = change_dependent(data, 'UPOSTAG', 'ADV', 'dep', 'advmod', 'advmod:phrase')
+    data = predicative_complements_to_xcomp(data)
+    data = change_dependent(data, 'UPOSTAG', 'ADV', 'DEPREL', 'dep', 'advmod', 'advmod:phrase')
     data = flip_aux_xcomp_for_modals(data)
+    data = advmod_phrase_to_fixed(data)
+    # data.drop(columns=['SENTENCE'])
     data.to_csv('fixed_%s' % filepath, sep='\t', index=False, header=False, quoting=csv.QUOTE_NONE)
 
-make_changes(training_treebank)
+make_changes(test_treebank)
 
-def inspect(data, dataSeries):
-    postags = []
-    relevant_heads = []
-    for i, v in dataSeries.iterrows():
-        context = get_context(data, v)
-        dependent = context[0]
-        head = context[1]
-        sentence = context[2]
-        if (dependent['UPOSTAG'] == 'ADV') and (head['DEPREL'] == 'advmod:phrase'):
-            relevant_heads.append(head['DEPREL'])
-            print(sentence)
-            print(dependent['INDEX'], dependent['FORM'], dependent['UPOSTAG'])
-            print(head['INDEX'], head['FORM'], head['UPOSTAG'])
-        postags.append(dependent['UPOSTAG'])
 
-    print(Counter(relevant_heads))
-    print(Counter(postags))
-
-    # if get_head(v)['col_1'] == 'some_value':
-    #     print("dependent", v['FORM'])
-    #     print("head", get_head(v)['FORM'])
-    #     print("head", get_head(get_head(v))['FORM'])
 
 
