92946bf1c2882871f34520407d2d096b5e2adbe4
shovalsa <shovatz@gmail.com>
2018-10-28 11:27:53 +0200

    last changes before freeze

--- changes_to_treebank.py
+++ changes_to_treebank.py
@@ -1,23 +1,105 @@
-import re
+"""
+conversions block #1  (2018-07-09) before article
+# replacing the first relation label with the second one.
+data = naive_change_value(data, 'DEPREL', 'iobj', 'obl')
+data = naive_change_value(data, 'DEPREL', 'acl:inf', 'acl')
+data = naive_change_value(data, 'DEPREL', 'det:quant', 'det')
+data = naive_change_value(data, 'UPOSTAG', 'PART', 'ADP')
+data = naive_change_value(data, 'XPOSTAG', 'PART', 'ADP')
+
+# some occurrences of ze / zo are labeled as amod/PRON and we think they shouldn't. Such are the determiners that appear after the nominal.
+data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'PRON', 'amod', new_col2_value='det')
+data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'PRON', 'amod', new_col2_value='det')
+
+# replacing the feature prefix=yes with the relation compound:affix
+data = change_COL1xFEATS(data, 'DEPREL', 'Prefix=Yes', new_col1_value="compound:affix")
+
+# changing past/future copulars from aux to cop and their POS to AUX.
+data = change_COL1xFEATS(data, 'DEPREL', 'VerbType=Cop', old_col1_value='aux', new_col1_value='cop')
+data = change_COL1xFEATS(data, 'UPOSTAG', 'VerbType=Cop', old_col1_value='VERB', new_col1_value='AUX')
+data = change_COL1xFEATS(data, 'XPOSTAG', 'VerbType=Cop', old_col1_value='VERB', new_col1_value='AUX')
+
+# Replacing hebrew specific labels with global ones for non-specific phenomena:
+data = naive_change_value(data, 'DEPREL', 'conj:discourse', 'parataxis')
+data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'ADV', 'obl:tmod', new_col2_value='advmod')
+data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'ADV', 'obl:tmod', new_col2_value='advmod')
+data = naive_change_value(data, 'DEPREL', 'advmod:inf', 'acl')
+data = naive_change_value(data, 'DEPREL', 'aux:q', 'mark:q')
+data = advmod_phrase_to_fixed(data)
+data = naive_change_value(data, 'DEPREL', 'advmod:phrase', 'advmod')
+
+# wherever the POS tag is INTJ, the label should be discourse, and not advmod.
+data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'INTJ', 'advmod', new_col2_value='discourse')
+data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'INTJ', 'advmod', new_col2_value='discourse')
+
+data = predicative_complements_to_xcomp(data)
+data = change_dependent(data, 'UPOSTAG', 'ADV', 'DEPREL', 'dep', 'advmod', 'advmod:phrase')
+
+# making modal items as aux (and respectively dependent) and passing their label to their (former) predicate dependent.
+data = flip_aux_xcomp_for_modals(data)
+
+"""
+
+
+"""
+Conversion block #2 (2018-10-28) before freeze
+
+
+    # fix instances where the case's form was different from the lemma, e.g. להם --> להם + הם instead of להם --> ל_ + הם
+    data.at[(data['UPOSTAG'] == 'ADP') & (data['FORM'] != data['LEMMA']) & (data['LEMMA'] == '_'), 'LEMMA'] = data['FORM']
+    for i, row in data[(data['UPOSTAG'] == 'ADP') & (data['FORM'] != data['LEMMA']) & (data['LEMMA'] != ('של')) & (data['LEMMA'] != ('את'))].iterrows():
+        if get_head(data, row)['LEMMA'] == 'הוא':
+            data.at[row.name, 'FORM'] = row['LEMMA'] + '_'
+
+
+    # Whereas the present tense existential markers were marked as HebExistential=True in the FEATS column (יש בו נגיעה לענייני החוק), past and future inflected instances were marked as VerbType=Cop (לא היה איש בדירתו)
+    # Whenever a verb is both marked as VerbType=cop, it is the head of its clause and it is followed by its subject, we changed the VerbType=Cop feature into HebExistential=True.
+    subjs = data[data['DEPREL'] == 'nsubj']
+    for i, row in subjs.iterrows():
+        head = get_head(data, row)
+        if 'VerbType=Cop' in head['FEATS'] and int(head['INDEX']) < int(row['INDEX']):
+            data.at[head.name, 'FEATS'] = "|".join(sorted(head['FEATS'].replace('VerbType=Cop', 'HebExistential=True').split("|")))
+
+
+    # change (CCONJ X advmod) to (ADV X advmod)
+    data = change_COL1xCOL2(data, 'FORM', 'DEPREL', 'לכן', 'cc', new_col2_value='advmod')
+    data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'CCONJ', 'advmod', new_col1_value='ADV')
+    data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'CCONJ', 'advmod', new_col1_value='ADV')
+
+    # For cases of אותו, אותה, אותן etc. in the sense of "the same", replace Case=Acc with Definite=Def and add אותו as lemma.
+    data.at[(data['FORM'].str.contains('אות', na=False)) & (data['FEATS'].str.contains('Case=Acc', na=False)), 'LEMMA'] = 'אותו'
+    data.at[(data['FORM'].str.contains('אות', na=False)) & (data['FEATS'].str.contains('Case=Acc', na=False)), 'FEATS'] = data['FEATS'].str.replace('Case=Acc', 'Definite=Def')
+
+    # change לראותם into לראות את הם --> modified in Victoria's code.
+    # hu = data[(data['FEATS'].str.contains('Case=Acc\|', na=False))]
+
+
+    # change fixed to advmod where internal structure makes the 'fixed' token come before its head.
+    for i, f in get_elements(data, 'DEPREL', 'fixed').iterrows():
+        if int(f['INDEX']) < int(get_head(data, f)['INDEX']):
+            data.at[f.name, 'DEPREL'] = 'advmod'
+
+
+"""
+
 import csv
 import pandas as pd
-import numpy as np
-from collections import Counter
-import re
+
 
 dev_treebank = 'he_htb-ud-dev.conllu'
 training_treebank = 'he_htb-ud-train.conllu'
 test_treebank = 'he_htb-ud-test.conllu'
+en_dev = 'english-dev'
 
 
 def suit_for_pandas(filepath):
     source = open(filepath, 'r')
     output = open("modified_%s" % filepath, 'w')
     output.write("INDEX	FORM	LEMMA	UPOSTAG	XPOSTAG	FEATS	HEAD	DEPREL	DEPS	MISC\n")
-    sent_id = ''
+    # sent_id = ''
     for line in source.readlines():
         if 'sent_id' in line:
-            sent_id = "".join([x for x in line if x.isnumeric()])
+            # sent_id = "".join([x for x in line if x.isnumeric()])
             output.write('\n')
         if line[0] == '#':
             output.write(line + "\t"*9 + '\n')
@@ -90,22 +172,6 @@ def get_head(data, dependent):
                     head = data.iloc[int(head.name) + 1]
     return head
 
-def get_context(data, dependent):
-    """
-    shows the head and the sentence of the given dependent
-    :param data:
-    :param dependent:
-    :return:
-    """
-    sentence = dependent
-    while '#' not in str(sentence['INDEX']):
-        sentence = data.iloc[int(sentence.name) - 1]
-    context = [dependent['FORM'], sentence['INDEX']]
-    return context
-
-
-
-
 
 def flip_aux_xcomp_for_modals(data):
     deps = get_elements(data, 'DEPREL', 'xcomp')
@@ -120,6 +186,8 @@ def flip_aux_xcomp_for_modals(data):
             data.loc[head.name, 'HEAD'] = v['INDEX']
             data.loc[i, 'HEAD'] = deps.loc[i, 'HEAD']
     return data
+
+
 
 def change_dependent(data, extract_col, extract_value, col, old_dependent_value, new_dependent_value, head_value):
     name = 0
@@ -145,16 +213,6 @@ def predicative_complements_to_xcomp(data):
     for index, v in adjs.iterrows():
         if get_head(data, v)['UPOSTAG'] == 'VERB' and v['DEPREL'] in ['advcl', 'advmod']:
             data.at[v.name, 'DEPREL'] = 'xcomp'
-    # verbs = get_elements(data, 'UPOSTAG', 'VERB')
-    # for index, v in verbs.iterrows():
-    #     if v['DEPREL'] == 'acl':
-    #         head = get_head(data, v)
-    #         head_of_head = get_head(data, head)
-    #         if head_of_head['UPOSTAG'] == 'VERB' and head['DEPREL'] == 'obj' and head['UPOSTAG'] == 'PRON':
-    #             print(get_context(data, v))
-    #             print(head_of_head['FORM'])
-    #             print(v['FORM'], v['DEPREL'])
-
     return data
 
 def advmod_phrase_to_fixed(data):
@@ -173,41 +231,6 @@ def advmod_phrase_to_fixed(data):
             data.at[v.name, 'DEPREL'] = 'fixed'
     return data
 
-def inspect(data):
-    roots = get_elements(data, 'DEPREL', 'root')
-    for i, v in roots.iterrows():
-        print(get_head(data, v)['FORM'], " ", v['FORM'])
-
-def make_changes(filepath):
-    suit_for_pandas(filepath)
-    data = pd.read_csv("modified_%s" % filepath, sep='\t', quoting=csv.QUOTE_NONE,  skip_blank_lines=True)
-    # data = add_sentence(data)
-    data = naive_change_value(data, 'DEPREL', 'iobj', 'obl')
-    data = naive_change_value(data, 'DEPREL', 'acl:inf', 'acl')
-    data = naive_change_value(data, 'DEPREL', 'det:quant', 'det')
-    data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'PRON', 'amod', new_col2_value='det')
-    data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'PRON', 'amod', new_col2_value='det')
-    data = change_COL1xFEATS(data, 'DEPREL', 'Prefix=Yes', new_col1_value="compound:affix")
-    data = change_COL1xFEATS(data, 'DEPREL', 'VerbType=Cop', old_col1_value='aux', new_col1_value='cop')
-    data = change_COL1xFEATS(data, 'UPOSTAG', 'VerbType=Cop', old_col1_value='VERB', new_col1_value='AUX')
-    data = change_COL1xFEATS(data, 'XPOSTAG', 'VerbType=Cop', old_col1_value='VERB', new_col1_value='AUX')
-    data = naive_change_value(data, 'DEPREL', 'conj:discourse', 'parataxis')
-    data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'ADV', 'obl:tmod', new_col2_value='advmod')
-    data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'ADV', 'obl:tmod', new_col2_value='advmod')
-    data = naive_change_value(data, 'DEPREL', 'advmod:inf', 'acl')
-    data = naive_change_value(data, 'DEPREL', 'aux:q', 'mark:q')
-    data = naive_change_value(data, 'UPOSTAG', 'PART', 'ADP')
-    data = naive_change_value(data, 'XPOSTAG', 'PART', 'ADP')
-    data = change_COL1xCOL2(data, 'UPOSTAG', 'DEPREL', 'INTJ', 'advmod', new_col2_value='discourse')
-    data = change_COL1xCOL2(data, 'XPOSTAG', 'DEPREL', 'INTJ', 'advmod', new_col2_value='discourse')
-    data = predicative_complements_to_xcomp(data)
-    data = change_dependent(data, 'UPOSTAG', 'ADV', 'DEPREL', 'dep', 'advmod', 'advmod:phrase')
-    data = flip_aux_xcomp_for_modals(data)
-    data = advmod_phrase_to_fixed(data)
-    data = naive_change_value(data, 'DEPREL', 'advmod:phrase', 'advmod')
-    # data.drop(columns=['SENTENCE'])
-    data.to_csv('modified_%s' % filepath, sep='\t', index=False, header=False, quoting=csv.QUOTE_NONE)
-    add_empty_lines(filepath)
 
 def add_empty_lines(filepath):
     newfile = open('fixed_%s' % filepath, 'w')
@@ -224,13 +247,46 @@ def add_empty_lines(filepath):
                 pass
             else:
                 newfile.write(line.strip() + '\n')
+    newfile.write("\n")
+    newfile.close()
+
+
+def make_changes(filepath):
+    suit_for_pandas(filepath)
+    data = pd.read_csv("modified_%s" % filepath, sep='\t', quoting=csv.QUOTE_NONE,  skip_blank_lines=True)
+    """
+    add changes here:
+    """
+    # make new or get from one of the blocks above.
+    """
+    end of periodical changes
+    """
+    data.to_csv('modified_%s' % filepath, sep='\t', index=False, header=False, quoting=csv.QUOTE_NONE)
+    add_empty_lines(filepath)
 
 
 
+def inspect(filepath):
+    header = ['INDEX', 'FORM', 'LEMMA', 'UPOSTAG', 'XPOSTAG', 'FEATS', 'HEAD', 'DEPREL', 'DEPS', 'MISC']
+    return pd.read_csv("modified_%s" % filepath, sep='\t', quoting=csv.QUOTE_NONE,  skip_blank_lines=True, names=header)
+
+
+def get_context(data, dependent):
+    """
+    shows the head and the sentence of the given dependent
+    :param data:
+    :param dependent:
+    :return:
+    """
+    sentence = dependent
+    while '#' not in str(sentence['INDEX']):
+        sentence = data.iloc[int(sentence.name) - 1]
+    context = [get_head(data, dependent)['FORM'], sentence['INDEX']]
+    return context
 
 if __name__ == "__main__":
-    make_changes(training_treebank)
-
+    # make_changes(dev_treebank)
+    data = inspect(dev_treebank)
 
 
 
